534004a8677268a66af15530827a97a170e22c41
Alexander Cooke <alex.cooke@live.com>
2016-12-03 22:22:45 +1000

    fix missing packages

--- source/index.spec.js
+++ source/index.spec.js
@@ -2,9 +2,12 @@ describe("first test", function () {
 
     it("should pass", function () {
 
+        //console.log(angular.mock);
+
+        var app = angular.mock.module("app");
+
         expect(1).toBe(1);
 
     });
-
 
 })
